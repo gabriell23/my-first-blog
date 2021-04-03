6d085f80c06779c73b592ae73216c8cd2c9727c9
gabriel <gabriel.belmiro@academico.ifpb.edu.br>
2021-04-03 14:46:54 -0300

    My Django Girls app, first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\gabri\onedrive\área de trabalho\blog\djangogirls\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
